b302c18345ef9bf2aef0e945ffdaabb0e8043c31
Brianna Garcia <147223105+Brianna1202Garcia@users.noreply.github.com>
2025-01-30 02:29:26 +0000

    done

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,15 @@
+public class BankAccount {
+    public static void main(String[] args) {
+        float startBalance = 2175.37f;
+        float newBalance = startBalance - 302.50f;
+        newBalance = newBalance + 50.03f;
+        newBalance = (newBalance / 2) + 20;
+        newBalance--;
+        newBalance = newBalance * 2;
+        newBalance++;
+        System.out.print(newBalance);
 
+
+
+    }
+}
